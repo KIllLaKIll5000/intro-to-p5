e4f39bf5153bb44caa530e09b8a257931d96e3df
Cbarnes <cbarnes@wlapcs.org>
2017-04-04 19:01:13 +0000

    lol

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+
+}
+
+function draw(){
+    fill(255,0,0)
+    rect(0,0,250,250);
+    stroke(132, 0, 0);
+    strokeWeight(3);
+    line(0,0,250,250);
+    fill(132, 0, 0);
+    
+    ellipse(250,260,150,100)
+    
+    ellipse(125,130,150,150);
+    line(250,250,250,400);
+    line(250,250,500,0);
+    rect(250,250,250,0);
+    rect(250,0,250,0);
+    
+    
+    
+    
+}
